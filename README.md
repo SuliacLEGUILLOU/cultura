98fbe36b482084ca7f0befc2acab63afbc29b3f4
Suliac LE-GUILLOU <sle-guil@e2r10p9.42.fr>
2018-01-20 15:22:52 +0100

    doc and formating update

--- README.md
+++ README.md
@@ -8,8 +8,14 @@ Cultura bot gets a random wikipedia article and post his summary on mastodon.
 It work with python3.
 I summon it with cron every hour.
 
+List of instances :
+fr - https://botsin.space/@cultura
+
 ## Credit and contribution
 
 This work is inspired from the thaega blaguesrandom bot : https://github.com/Thaega/blaguesrandom
 
-PR are welcome
+If you open your own instance, I would gladly add you in the list.
+I plain to open English, German and Japanese account pretty soon.
+
+Also remmeber that one post per hour is great for both the api use in this project.
--- main.py
+++ main.py
@@ -6,20 +6,16 @@ exec(f.read(), globals())
 f.close()
 
 wikipedia.set_lang('fr')
-
 page = wikipedia.page(wikipedia.random())
-
 text = '%s\n\n%s' % (page.summary, page.url)
-
 
 shorten_summary = page.summary
 while len(text) > 500:
     print('CUT')
     shorten_summary = shorten_summary.rsplit('.', 1)[0]
     text = '%s\n\n%s' % (shorten_summary, page.url)
-
 print(text)
-print(len(text))
+print('length:', len(text))
 
 masto = Mastodon(
     client_id = 'pytooter_clientcred.secret',
@@ -32,5 +28,4 @@ masto.log_in(
     mastodon_password,
     to_file = 'pytooter_usercred.secret'
 )
-
 masto.status_post(text)
